465eb79280137511f441ce2e64610b5eeb79250f
Carson Fox <carson.fox@atomicjolt.com>
2020-11-08 23:14:50 -0500

    Testing different problem sizes

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
-
 make
 
+for i in 1 2 4 8
+  echo "Using" $i "processes:"
+  time mpiexec -n $i ./Histogram 128 0.0 5.0 268435456 > /dev/null
+end
+
 # Histogram bins min max data
-mpiexec -n 2 ./Histogram 10 0.0 5.0 100
+#mpiexec -n 2 ./Histogram 10 0.0 5.0 100
